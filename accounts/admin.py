b6ac80ac495f2dd36639667ffcf7d71bb87d5330
Emma Guilbault <emma.guilbault@free.fr>
2026-01-07 12:52:54 +0100

    configure user admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Account)
+
+@admin.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = ["email", "first_name", "last_name", "is_admin"]
+    list_filter = []
+    search_fields = ["email", "first_name", "last_name"]
+    ordering = ["-date_joined"]
+
+    readonly_fields = ["last_login", "date_joined"]
+
+    filter_horizontal = []
+    fieldsets = []
