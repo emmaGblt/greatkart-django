55a3b94a2e7ff6745c34cb2cddaaad5392d9a4cb
Emma Guilbault <emma.guilbault@free.fr>
2026-01-08 12:53:17 +0100

    create Product model and configure its admin page

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "stock", "category", "updated_at", "is_available"]
+
+    prepopulated_fields = {"slug": ["name"]}
