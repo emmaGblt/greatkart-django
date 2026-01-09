5545f28314b8bfa32b8668fe46847a8263c15683
Emma Guilbault <emma.guilbault@free.fr>
2026-01-09 15:00:02 +0100

    create Cart and CartItem models

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from store.models import Product
 
-# Create your models here.
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"${self.product.name} (cart: ${self.cart.id})"
