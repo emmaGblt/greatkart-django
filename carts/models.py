48bfe1f12ef711618624625945cb92fc55872e1b
Emma Guilbault <emma.guilbault@free.fr>
2026-01-09 15:23:36 +0100

    add a session_key field to the Cart model

--- carts/models.py
+++ carts/models.py
@@ -3,6 +3,7 @@ from store.models import Product
 
 
 class Cart(models.Model):
+    session_key = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
