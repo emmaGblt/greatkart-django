60a0a1e4da66e526a3898db79b1a8d4a24117608
Emma Guilbault <emma.guilbault@free.fr>
2026-01-10 14:30:01 +0100

    show if product is already in the cart in product detail template

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.views import _get_session_key
+from carts.models import CartItem
 
 
 def store(request, category_slug=None):
@@ -21,7 +23,11 @@ def store(request, category_slug=None):
 
 def product_detail(request, category_slug=None, product_slug=None):
     product = get_object_or_404(Product, slug=product_slug)
+    session_key = _get_session_key(request)
+    is_already_in_cart = CartItem.objects.filter(
+        cart__session_key=session_key, product=product
+    ).exists()
 
-    context = {"product": product}
+    context = {"product": product, "is_already_in_cart": is_already_in_cart}
 
     return render(request, "store/product_detail.html", context)
