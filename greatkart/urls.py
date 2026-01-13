ca0bbeba9668f3bdeec3d3c2a1b1be587df72d43
Emma Guilbault <emma.guilbault@free.fr>
2026-01-13 16:00:52 +0100

    add accounts app urls to the project router

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -25,6 +25,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
-    path("store/", include("store.urls")),
+    path("accounts/", include("accounts.urls")),
     path("cart/", include("carts.urls")),
+    path("store/", include("store.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
