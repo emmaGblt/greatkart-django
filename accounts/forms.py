d7d22b0e143ecfad3bddd939d5b62a7402f2c51a
Emma Guilbault <emma.guilbault@free.fr>
2026-01-20 11:46:50 +0100

    update the registration model form and the corresponding template

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,29 @@ from .models import Account
 
 
 class RegistrationForm(forms.ModelForm):
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm password"})
+    )
+
     class Meta:
         model = Account
-        fields = ["first_name", "last_name", "phone_number", "email", "password"]
+        fields = [
+            "first_name",
+            "last_name",
+            "phone_number",
+            "email",
+            "password",
+            "confirm_password",
+        ]
+        widgets = {
+            "password": forms.PasswordInput(attrs={"placeholder": "Enter password"}),
+            "first_name": forms.TextInput(attrs={"placeholder": "Fist name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last name"}),
+            "email": forms.TextInput(attrs={"placeholder": "Email"}),
+            "phone_number": forms.TextInput(attrs={"placeholder": "Phone number"}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
