7771a3ecc3fb194cc39fef472470acfcd2e1cf55
Emma Guilbault <emma.guilbault@free.fr>
2026-01-21 15:56:07 +0100

    create account verification email template + update register view to send verification email

--- accounts/views.py
+++ accounts/views.py
@@ -4,22 +4,44 @@ from .models import Account
 from django.contrib import messages, auth
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.utils.http import urlsafe_base64_encode
 
 
 def register(request):
     if request.method == "POST":
-        print("here")
         form = form = RegistrationForm(request.POST)
         if form.is_valid():
+            # GET THE DATA FROM THE FORM
             first_name = form.cleaned_data["first_name"]
             last_name = form.cleaned_data["last_name"]
             phone_number = form.cleaned_data["phone_number"]
             email = form.cleaned_data["email"]
             password = form.cleaned_data["password"]
 
+            # CREATE A USER
             user = Account.objects.create_user(first_name, last_name, email, password)
             user.phone_number = phone_number
             user.save()
+
+            # SEND A LINK TO ACTIVATE THE USER (= EMAIL ADDRESS VALIDATION)
+            current_site = get_current_site(request)
+            mail_subject = "Account activation"
+            mail_message = render_to_string(
+                "accounts/verification_email.html",
+                {
+                    "user": user,
+                    "domain": current_site,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": default_token_generator.make_token(user),
+                },
+            )
+            recipient_email = email
+            send_mail(mail_subject, mail_message, recipient_list=[recipient_email])
 
             messages.success(request, "Registration successful!")
             return redirect(reverse("register"))
