72a75bfcda1547af1e2e7ee52dd0b2bf4c614fcc
Emma Guilbault <emma.guilbault@free.fr>
2026-01-21 17:16:18 +0100

    fix account activation url

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,5 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
-    path(
-        "activate/<str:uidb64>/<str:token>",
-        views.activate_account,
-        name="activate_account",
-    ),
+    path("activate/<uidb64>/<token>/", views.activate_account, name="activate_account"),
 ]
