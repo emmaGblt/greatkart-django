a64c59d2a49a9713c9687460ce561bfca4400c8b
Emma Guilbault <emma.guilbault@free.fr>
2026-01-27 11:19:40 +0100

    create order model form

---
+++ orders/forms.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from .models import Order
+
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "address_line_1",
+            "address_line_2",
+            "city",
+            "state",
+            "country",
+            "note",
+        ]
