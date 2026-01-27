2ae547692513bef7ec3b258ad7b1ca22bc1bd602
Emma Guilbault <emma.guilbault@free.fr>
2026-01-27 12:43:34 +0100

    fix order phone number max length

--- orders/models.py
+++ orders/models.py
@@ -36,7 +36,7 @@ class Order(models.Model):
     )
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=60)
+    phone_number = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     address_line_1 = models.CharField(max_length=50)
     address_line_2 = models.CharField(max_length=50, blank=True)
