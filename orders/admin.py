7afb798d4e30caf7ee6d9093e69a106afeb98d7e
Emma Guilbault <emma.guilbault@free.fr>
2026-01-27 16:37:22 +0100

    update orders admin page

--- orders/admin.py
+++ orders/admin.py
@@ -9,7 +9,29 @@ class PaymentAdmin(admin.ModelAdmin):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ["reference", "created_at", "updated_at"]
+    fieldsets = [
+        (None, {"fields": ["reference", "user", "payment", "status"]}),
+        (
+            "Shipping info",
+            {
+                "fields": [
+                    "first_name",
+                    "last_name",
+                    "phone_number",
+                    "email",
+                    "address_line_1",
+                    "address_line_2",
+                    "city",
+                    "state",
+                    "country",
+                    "note",
+                ]
+            },
+        ),
+        ("Billing info", {"fields": ["total", "tax"]}),
+        ("Other", {"fields": ["ip", "created_at", "updated_at"]}),
+    ]
 
 
 @admin.register(OrderProduct)
