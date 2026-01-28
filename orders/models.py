7957afcb8ba004a621fa6c98fb0313536d8adbf0
Emma Guilbault <emma.guilbault@free.fr>
2026-01-28 11:44:45 +0100

    add 2 model properties to order model

--- orders/models.py
+++ orders/models.py
@@ -53,11 +53,19 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_at"]
+
     def __str__(self):
         return "{0} ({1} {2})".format(self.reference, self.first_name, self.last_name)
 
-    class Meta:
-        ordering = ["-created_at"]
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_address(self):
+        return f"{self.address_line_1} {self.address_line_2}"
 
 
 class OrderProduct(models.Model):
