2740be9e5c395c9ebf9afcd8a1a1725a7239bebd
Emma Guilbault <emma.guilbault@free.fr>
2026-02-01 12:49:03 +0100

    add ProductReview admin page

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Variation
+from .models import Product, ProductReview, Variation
 
 
 @admin.register(Product)
@@ -14,3 +14,8 @@ class VariationAdmin(admin.ModelAdmin):
     list_display = ["product", "category", "value", "is_active"]
     list_filter = ["category", "is_active"]
     list_editable = ["is_active"]
+
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    pass
