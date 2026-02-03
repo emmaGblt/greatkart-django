48cccd347a917b6907da83cb3438290ae4697256
Emma Guilbault <emma.guilbault@free.fr>
2026-02-03 13:52:59 +0100

    change ordering of product reviews

--- orders/models.py
+++ orders/models.py
@@ -89,4 +89,4 @@ class OrderProduct(models.Model):
         return "{0} (order: {1})".format(self.product.name, self.order.reference)
 
     class Meta:
-        ordering = ["-created_at"]
+        ordering = ["-updated_at"]
